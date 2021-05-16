3cf3c58f32aafdd37708ffc99c24be482c28607f
nonnonstop <42905588+nonnonstop@users.noreply.github.com>
2021-05-16 11:56:10 +0900

    Fix type annotation of viewer script

--- app/src/main/script/src/viewer.ts
+++ app/src/main/script/src/viewer.ts
@@ -4,7 +4,7 @@
  * This function is called from LoadActivity on UI thread.
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function view(activity: Activity, htmlUrl: string) {
+function view(activity: Activity, htmlUrl: string): void {
     activity.getSystemService('activity')
         .killBackgroundProcesses('jp.co.airfront.android.a2chMate');
     activity.finish();
