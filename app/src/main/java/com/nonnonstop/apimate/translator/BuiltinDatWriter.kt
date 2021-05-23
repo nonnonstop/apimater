1f791828309f0a4f53542cd22afbc93a3a90292c
nonnonstop <42905588+nonnonstop@users.noreply.github.com>
2021-05-23 10:00:14 +0900

    Modify dat writer * Improve performance when a lot of files exist in the directory

--- app/src/main/java/com/nonnonstop/apimate/translator/BuiltinDatWriter.kt
+++ app/src/main/java/com/nonnonstop/apimate/translator/BuiltinDatWriter.kt
@@ -1,35 +1,26 @@
 package com.nonnonstop.apimate.translator
 
-import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
+import android.provider.DocumentsContract
 import androidx.documentfile.provider.DocumentFile
 import java.io.BufferedWriter
 import java.io.IOException
 import java.io.OutputStream
 import java.io.OutputStreamWriter
 
-class BuiltinDatWriter(context: Context) : IDatWriter {
-    private val baseDir: DocumentFile
-    private val contentResolver: ContentResolver
+class BuiltinDatWriter(private val context: Context) : IDatWriter {
+    private val treeUri = context
+        .getSharedPreferences("DatWriter", Context.MODE_PRIVATE)
+        .getString("documentTreeUri", null)
+        ?.let {
+            Uri.parse(it)
+        }
+        ?: throw IOException("Dat directory is not initialized")
     private var datInfo: IDatInfo? = null
     private var outputStream: OutputStream? = null
     private var outputStreamWriter: OutputStreamWriter? = null
     private var bufferedWriter: BufferedWriter? = null
-
-    init {
-        val baseUri = context
-            .getSharedPreferences("DatWriter", Context.MODE_PRIVATE)
-            .getString("documentTreeUri", null)
-            ?.let {
-                Uri.parse(it)
-            }
-            ?: throw IOException("Dat directory is not initialized")
-        baseDir = DocumentFile
-            .fromTreeUri(context, baseUri)
-            ?: throw IOException("Failed to open dat directory")
-        contentResolver = context.contentResolver
-    }
 
     override fun open(info: IDatInfo) {
         datInfo = info
@@ -39,11 +30,19 @@ class BuiltinDatWriter(context: Context) : IDatWriter {
         if (bufferedWriter != null)
             return
         val filename = datInfo?.filename ?: throw IOException("Failed to get dat info")
-        val file = baseDir.findFile(filename)
-            ?: baseDir.createFile("application/octet-stream", filename)
-            ?: throw IOException("Failed to create dat file")
+        val documentUri = DocumentsContract.buildDocumentUriUsingTree(
+            treeUri, "${DocumentsContract.getTreeDocumentId(treeUri)}/${filename}"
+        )
+        val documentFile = DocumentFile.fromSingleUri(context, documentUri)
+            ?: throw IOException("Failed to get dat file")
+        if (!documentFile.exists()) {
+            val documentDir = DocumentFile.fromTreeUri(context, treeUri)
+                ?: throw IOException("Failed to get dat directory")
+            documentDir.createFile("application/octet-stream", filename)
+                ?: throw IOException("Failed to create dat file")
+        }
         try {
-            outputStream = contentResolver.openOutputStream(file.uri, "wt")
+            outputStream = context.contentResolver.openOutputStream(documentUri, "wt")
                 ?: throw IOException("Failed to open dat file")
             outputStreamWriter = outputStream!!.writer(Charsets.MS932)
             bufferedWriter = outputStreamWriter!!.buffered()
